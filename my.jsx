2d033cd106d05e2de1a81ea8f99a3a8d4049ec10
gauravkumar88-git <gauravkumarcse88@gmail.com>
2025-09-03 17:10:19 +0530

    v9

--- my.jsx
+++ my.jsx
@@ -4,9 +4,8 @@ const my = () => {
   return (
     <div>
       <h1>hello</h1>
-      <h2>my name is gaurav
-        
-      </h2>
+      <h2>my name is gaurav </h2>
+      <h3>gaurav parp</h3>
     </div>
   )
 }
